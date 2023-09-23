09c57b236102d8ee0194d7578c59710c0e606947
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2023-09-23 19:55:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+number = 0;
+
+function next(){
+    number=Math.round(Math.random()*31)+1;
+    console.log(number);
+    if(number == 1){
+        document.getElementById("image").src="1.png";
+            }
+    if(number == 2){
+        document.getElementById("image").src="2.png";
+                    }
+    if(number == 3){
+        document.getElementById("image").src="3.png";
+                            }
+    if(number == 4){
+        document.getElementById("image").src="4.png";
+         }
+         if(number == 5){
+            document.getElementById("image").src="5.png";
+             }
+             if(number == 6){
+                document.getElementById("image").src="6.png";
+                 }
+                 if(number == 7){
+                    document.getElementById("image").src="7.png";
+                     }
+                     if(number == 8){
+                        document.getElementById("image").src="8.png";
+                         }
+                         if(number == 9){
+                            document.getElementById("image").src="9.png";
+                             }
+                             if(number == 10){
+                                document.getElementById("image").src="10.png";
+                                 }
+                                 if(number == 11){
+                                    document.getElementById("image").src="11.png";
+                                     }
+                                     if(number == 12){
+                                        document.getElementById("image").src="12.png";
+                                         }
+                                         if(number == 13){
+                                            document.getElementById("image").src="13.png";
+                                             }
+                                             if(number == 14){
+                                                document.getElementById("image").src="14.png";
+                                                 }
+                                                 if(number == 15){
+                                                    document.getElementById("image").src="15.png";
+                                                     }
+                                                     if(number == 16){
+                                                        document.getElementById("image").src="16.png";
+                                                         }
+                                                         if(number == 17){
+                                                            document.getElementById("image").src="17.png";
+                                                             }
+                                                             if(number == 18){
+                                                                document.getElementById("image").src="18.png";
+                                                                 }
+                                                                 if(number == 19){
+                                                                    document.getElementById("image").src="19.png";
+                                                                     }
+                                                                     if(number == 20){
+                                                                        document.getElementById("image").src="20.png";
+                                                                         }
+                                                                         if(number == 21){
+                                                                            document.getElementById("image").src="21.png";
+                                                                             }
+                                                                             if(number == 22){
+                                                                                document.getElementById("image").src="22.png";
+                                                                                 }
+                                                                                 if(number == 23){
+                                                                                    document.getElementById("image").src="23.png";
+                                                                                     }
+                                                                                     if(number == 24){
+                                                                                        document.getElementById("image").src="24.png";
+                                                                                         }
+                                                                                         if(number == 25){
+                                                                                            document.getElementById("image").src="25.png";
+                                                                                             }
+                                                                                             if(number == 26){
+                                                                                                document.getElementById("image").src="26.png";
+                                                                                                 }
+                                                                                                 if(number == 27){
+                                                                                                    document.getElementById("image").src="27.png";
+                                                                                                     }
+                                                                                                     if(number == 28){
+                                                                                                        document.getElementById("image").src="28.png";
+                                                                                                         }
+                                                                                                         if(number == 29){
+                                                                                                            document.getElementById("image").src="29.png";
+                                                                                                             }
+                                                                                                             if(number == 30){
+                                                                                                                document.getElementById("image").src="30.png";
+                                                                                                                 }
+                                                                                                                 if(number == 31){
+                                                                                                                    document.getElementById("image").src="31.png";
+                                                                                                                     }
+                                                                                                                    }
+                if(number == 31){
+                disable=false;    
+                }
